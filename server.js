56ff0ed408a8c8f631bd686736ddf285fd7ff729
kelex345 <charlotteavery206@gmail.com>
2026-09-06 21:21:44 +0300

    Revert server.js to serve public/index.html

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ let entries = [];
 
 // Serve static files
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.get('/admin', (req, res) => {
